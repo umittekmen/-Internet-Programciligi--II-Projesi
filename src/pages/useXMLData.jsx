6d80144b2755e643c7e6dd0e603539e771f6344e
umittekmen <uumit380@gmail.com>
2024-05-02 21:53:10 +0300

    Değişiklik deneme

--- src/pages/useXMLData.jsx
+++ src/pages/useXMLData.jsx
@@ -7,14 +7,14 @@ function useXMLData() {
     useEffect(() => {
         // JSON dosyasından veriyi alma işlemi
         fetch('./data/data.json')
-            // Örnek olarak, verinin bulunduğu yolu belirtiyoruz
+
             .then(response => response.json())
             .then(data => {
-                // Veriyi alırken gerekli işlemleri yapabiliriz
+
                 setData(data);
             })
             .catch(error => {
-                // Hata durumunda gerekli işlemleri yapabiliriz
+
                 console.error('Veri alınamadı:', error);
             });
 
